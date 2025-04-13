c058ed99df3d28be049f957d6bd423c122009d1b
Hamzah Baig <baighamzah3@gmail.com>
2025-04-13 15:21:44 -0400

    Allow frontend Render URL in Vite preview config

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0'
+    port: 3000
   },
   preview: {
     host: '0.0.0.0',
-    port: 10000
+    port: 10000,
+    allowedHosts: ['mlb-frontend-3.onrender.com']
   }
 })
